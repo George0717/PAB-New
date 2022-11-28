af757cb242928f376098929815c6068c08015fa8
George0717 <raden7george@gmail.com>
2022-11-28 11:22:24 +0700

    3 push

--- aplikasiKu/app/src/main/java/com/if3b/aplikasiaku/splashActivity.java
+++ aplikasiKu/app/src/main/java/com/if3b/aplikasiaku/splashActivity.java
@@ -2,7 +2,10 @@ package com.if3b.aplikasiaku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class splashActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler hdl = new Handler();
+
+        hdl.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
